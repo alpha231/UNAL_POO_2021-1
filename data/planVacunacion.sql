851d77c4a2c21de392efaa2de513693814e13e1d
Juan J <juanjgarcialeon@gmail.com>
2021-08-02 03:21:56 -0500

    actualizacion de archivos para llenar la DB

--- data/planVacunacion.sql
+++ data/planVacunacion.sql
@@ -1,4 +1,4 @@
-INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('1', '60', '150', '2021-07-01', '2021-08-01');
-INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('2', '40', '59', '2021-08-01', '2021-09-01');
-INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('3', '20', '39', '2021-09-01', '2021-10-01');
-INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('4', '10', '19', '2021-10-01', '2021-11-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('1', '60', '150', '2021-09-01', '2021-10-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('2', '40', '59', '2021-10-01', '2021-11-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('3', '20', '39', '2021-11-01', '2021-12-01');
+INSERT INTO "main"."plan_vacunacion" ("idPlan", "edadMinima", "edadMaxima", "fechaInicio", "fechaFinal") VALUES ('4', '10', '19', '2021-12-01', '2022-01-01');
